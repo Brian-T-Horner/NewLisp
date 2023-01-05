fd9784f280dfa0e7bd2b995b0047e88a32d01840
Brian Horner <bhorner@suffolk.edu>
2023-01-04 22:54:27 -0500

    Added basic REPL for Lisp

--- main.c
+++ main.c
@@ -1,15 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void print(int n){
-    do{
-        puts("Hello World!"); //Short for put string
-        n--;
-    }while(n>0);
+// If on Windows compile these functions
+#ifdef _WIN64
+#include <string.h>
+
+/* Declaring a buffer array for string input */
+static char buffer[2048];
+
+// Fake readline function for testing
+char* readline(char* prompt){
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = '\0';
+    return cpy;
 }
 
-int main(int argc, char** argv) {
+void add_history(char* unused){}
 
-    int i = 5;
-    print(i);
+// Otherwise include the edit line headers
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
+
+int main(int argc, char** argv) {
+    puts ("Lispy Version 0.0.0.0.1");
+    puts("Press Ctrl+C to Exit.\n");
+
+
+    // Looping for input (Infinite loop)
+
+    while(1){
+
+        // Now in either case readline will be correctly defined
+        char* input = readline("lispy>");
+        add_history(input);
+
+        // Echo input back to user
+        printf("No you're a %f\n", input);
+        free(input);
+    }
     return 0;
 }
